07769ae59e50544028c920ef9aebf77ab00afe6a
truong chinh <doducchinhit@gmail.com>
2020-12-31 17:50:29 +0700

    add configured for all class

--- eShopSolution.Data/Entities/AppUser.cs
+++ eShopSolution.Data/Entities/AppUser.cs
@@ -1,11 +1,11 @@
-﻿// using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace eShopSolution.Data.Entities
 {
-    public class AppUser// : IdentityUser<Guid>
+    public class AppUser : IdentityUser<Guid>
     {
         public string FirstName { get; set; }
 
