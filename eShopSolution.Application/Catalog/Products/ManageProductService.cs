81fe373d5c2d8cbf8b1b03f4a2b3f954e8813a27
truong chinh <doducchinhit@gmail.com>
2021-01-02 15:28:53 +0700

    Add web api

--- eShopSolution.Application/Catalog/Products/ManageProductService.cs
+++ eShopSolution.Application/Catalog/Products/ManageProductService.cs
@@ -3,8 +3,6 @@ using eShopSolution.Data.EF;
 using eShopSolution.Data.Entities;
 using eShopSolution.Utilities.Exceptions;
 using eShopSolution.ViewModels.Catalog.Products;
-using eShopSolution.ViewModels.Catalog.Products.Manage;
-using eShopSolution.ViewModels.Catalog.Products.Public;
 using eShopSolution.ViewModels.Common;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
@@ -99,7 +97,7 @@ namespace eShopSolution.Application.Catalog.Products
             return await _context.SaveChangesAsync();
         }
 
-        public async Task<PagedResult<ProductViewModel>> GetAllPaging(ViewModels.Catalog.Products.Manage.GetProductPagingRequest request)
+        public async Task<PagedResult<ProductViewModel>> GetAllPaging(GetManageProductPagingRequest request)
         {
             //1. Select join
             var query = from p in _context.Products
---
+++ eShopSolution.BackendApi/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eShopSolution.Application.Catalog.Products;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShopSolution.BackendApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductController : ControllerBase
+	{
+		private readonly IPublicProductService _pubProductService;
+		public ProductController(IPublicProductService publicProductService)
+		{
+			this._pubProductService = publicProductService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Get()
+		{
+			var product = await this._pubProductService.GetAll();
+			return Ok(product);
+		}
+	}
+}
