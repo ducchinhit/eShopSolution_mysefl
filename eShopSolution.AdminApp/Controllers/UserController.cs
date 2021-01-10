f025b6bc423425450735e80356d8d6f7adaba975
truong chinh <doducchinhit@gmail.com>
2021-01-10 15:03:14 +0700

    already commited in lession 32

--- eShopSolution.AdminApp/Controllers/UserController.cs
+++ eShopSolution.AdminApp/Controllers/UserController.cs
@@ -17,134 +17,134 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace eShopSolution.AdminApp.Controllers
 {
-    public class UserController : BaseController
-    {
-        private readonly IUserApiClient _userApiClient;
-        private readonly IConfiguration _configuration;
+	public class UserController : BaseController
+	{
+		private readonly IUserApiClient _userApiClient;
+		private readonly IConfiguration _configuration;
 
-        public UserController(IUserApiClient userApiClient, IConfiguration configuration)
-        {
-            _userApiClient = userApiClient;
-            _configuration = configuration;
-        }
+		public UserController(IUserApiClient userApiClient, IConfiguration configuration)
+		{
+			_userApiClient = userApiClient;
+			_configuration = configuration;
+		}
 
-        public async Task<IActionResult> Index(string keyword, int pageIndex = 1, int pageSize = 10)
-        {
-            var request = new GetUserPagingRequest()
-            {
-                Keyword = keyword,
-                PageIndex = pageIndex,
-                PageSize = pageSize
-            };
-            var data = await _userApiClient.GetUsersPagings(request);
-            ViewBag.Keyword = keyword;
-            if (TempData["result"] != null)
-            {
-                ViewBag.SuccessMsg = TempData["result"];
-            }
-            return View(data.ResultObj);
-        }
+		public async Task<IActionResult> Index(string keyword, int pageIndex = 1, int pageSize = 10)
+		{
+			var request = new GetUserPagingRequest()
+			{
+				Keyword = keyword,
+				PageIndex = pageIndex,
+				PageSize = pageSize
+			};
+			var data = await _userApiClient.GetUsersPagings(request);
+			ViewBag.Keyword = keyword;
+			if (TempData["result"] != null)
+			{
+				ViewBag.SuccessMsg = TempData["result"];
+			}
+			return View(data.ResultObj);
+		}
 
-        [HttpGet]
-        public async Task<IActionResult> Details(Guid id)
-        {
-            var result = await _userApiClient.GetById(id);
-            return View(result.ResultObj);
-        }
+		[HttpGet]
+		public async Task<IActionResult> Details(Guid id)
+		{
+			var result = await _userApiClient.GetById(id);
+			return View(result.ResultObj);
+		}
 
-        [HttpGet]
-        public IActionResult Create()
-        {
-            return View();
-        }
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> Create(RegisterRequest request)
-        {
-            if (!ModelState.IsValid)
-                return View();
+		[HttpPost]
+		public async Task<IActionResult> Create(RegisterRequest request)
+		{
+			if (!ModelState.IsValid)
+				return View();
 
-            var result = await _userApiClient.RegisterUser(request);
-            if (result.IsSuccessed)
-            {
-                TempData["result"] = "Thêm mới người dùng thành công";
-                return RedirectToAction("Index");
-            }
+			var result = await _userApiClient.RegisterUser(request);
+			if (result.IsSuccessed)
+			{
+				TempData["result"] = "Thêm mới người dùng thành công";
+				return RedirectToAction("Index");
+			}
 
-            ModelState.AddModelError("", result.Message);
-            return View(request);
-        }
+			ModelState.AddModelError("", result.Message);
+			return View(request);
+		}
 
-        [HttpGet]
-        public async Task<IActionResult> Edit(Guid id)
-        {
-            var result = await _userApiClient.GetById(id);
-            if (result.IsSuccessed)
-            {
-                var user = result.ResultObj;
-                var updateRequest = new UserUpdateRequest()
-                {
-                    Dob = user.Dob,
-                    Email = user.Email,
-                    FirstName = user.FirstName,
-                    LastName = user.LastName,
-                    PhoneNumber = user.PhoneNumber,
-                    Id = id
-                };
-                return View(updateRequest);
-            }
-            return RedirectToAction("Error", "Home");
-        }
+		[HttpGet]
+		public async Task<IActionResult> Edit(Guid id)
+		{
+			var result = await _userApiClient.GetById(id);
+			if (result.IsSuccessed)
+			{
+				var user = result.ResultObj;
+				var updateRequest = new UserUpdateRequest()
+				{
+					Dob = user.Dob,
+					Email = user.Email,
+					FirstName = user.FirstName,
+					LastName = user.LastName,
+					PhoneNumber = user.PhoneNumber,
+					Id = id
+				};
+				return View(updateRequest);
+			}
+			return RedirectToAction("Error", "Home");
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> Edit(UserUpdateRequest request)
-        {
-            if (!ModelState.IsValid)
-                return View();
+		[HttpPost]
+		public async Task<IActionResult> Edit(UserUpdateRequest request)
+		{
+			if (!ModelState.IsValid)
+				return View();
 
-            var result = await _userApiClient.UpdateUser(request.Id, request);
-            if (result.IsSuccessed)
-            {
-                TempData["result"] = "Cập nhật người dùng thành công";
-                return RedirectToAction("Index");
-            }
+			var result = await _userApiClient.UpdateUser(request.Id, request);
+			if (result.IsSuccessed)
+			{
+				TempData["result"] = "Cập nhật người dùng thành công";
+				return RedirectToAction("Index");
+			}
 
-            ModelState.AddModelError("", result.Message);
-            return View(request);
-        }
+			ModelState.AddModelError("", result.Message);
+			return View(request);
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> Logout()
-        {
-            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            HttpContext.Session.Remove("Token");
-            return RedirectToAction("Index", "Login");
-        }
+		[HttpPost]
+		public async Task<IActionResult> Logout()
+		{
+			await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+			HttpContext.Session.Remove("Token");
+			return RedirectToAction("Index", "Login");
+		}
 
-        [HttpGet]
-        public IActionResult Delete(Guid id)
-        {
-            return View(new UserDeleteRequest()
-            {
-                Id = id
-            });
-        }
+		[HttpGet]
+		public IActionResult Delete(Guid id)
+		{
+			return View(new UserDeleteRequest()
+			{
+				Id = id
+			});
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> Delete(UserDeleteRequest request)
-        {
-            if (!ModelState.IsValid)
-                return View();
+		[HttpPost]
+		public async Task<IActionResult> Delete(UserDeleteRequest request)
+		{
+			if (!ModelState.IsValid)
+				return View();
 
-            var result = await _userApiClient.Delete(request.Id);
-            if (result.IsSuccessed)
-            {
-                TempData["result"] = "Xóa người dùng thành công";
-                return RedirectToAction("Index");
-            }
+			var result = await _userApiClient.Delete(request.Id);
+			if (result.IsSuccessed)
+			{
+				TempData["result"] = "Xóa người dùng thành công";
+				return RedirectToAction("Index");
+			}
 
-            ModelState.AddModelError("", result.Message);
-            return View(request);
-        }
-    }
+			ModelState.AddModelError("", result.Message);
+			return View(request);
+		}
+	}
 }
