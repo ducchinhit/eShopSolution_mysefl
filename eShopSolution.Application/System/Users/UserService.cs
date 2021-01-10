05d79a81a545cff959a8933a2c92a74bbb883c06
truong chinh <doducchinhit@gmail.com>
2021-01-10 11:34:45 +0700

    Delete User

--- eShopSolution.Application/System/Users/UserService.cs
+++ eShopSolution.Application/System/Users/UserService.cs
@@ -15,157 +15,171 @@ using System.Threading.Tasks;
 
 namespace eShopSolution.Application.System.Users
 {
-    public class UserService : IUserService
-    {
-        private readonly UserManager<AppUser> _userManager;
-        private readonly SignInManager<AppUser> _signInManager;
-        private readonly RoleManager<AppRole> _roleManager;
-        private readonly IConfiguration _config;
+	public class UserService : IUserService
+	{
+		private readonly UserManager<AppUser> _userManager;
+		private readonly SignInManager<AppUser> _signInManager;
+		private readonly RoleManager<AppRole> _roleManager;
+		private readonly IConfiguration _config;
 
-        public UserService(UserManager<AppUser> userManager,
-            SignInManager<AppUser> signInManager,
-            RoleManager<AppRole> roleManager,
-            IConfiguration config)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _roleManager = roleManager;
-            _config = config;
-        }
+		public UserService(UserManager<AppUser> userManager,
+			SignInManager<AppUser> signInManager,
+			RoleManager<AppRole> roleManager,
+			IConfiguration config)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+			_roleManager = roleManager;
+			_config = config;
+		}
 
-        public async Task<ApiResult<string>> Authencate(LoginRequest request)
-        {
-            var user = await _userManager.FindByNameAsync(request.UserName);
-            if (user == null) return null;
+		public async Task<ApiResult<string>> Authencate(LoginRequest request)
+		{
+			var user = await _userManager.FindByNameAsync(request.UserName);
+			if (user == null) return null;
 
-            var result = await _signInManager.PasswordSignInAsync(user, request.Password, request.RememberMe, true);
-            if (!result.Succeeded)
-            {
-                return new ApiErrorResult<string>("Đăng nhập không đúng");
-            }
-            var roles = await _userManager.GetRolesAsync(user);
-            var claims = new[]
-            {
-                new Claim(ClaimTypes.Email,user.Email),
-                new Claim(ClaimTypes.GivenName,user.FirstName),
-                new Claim(ClaimTypes.Role, string.Join(";",roles)),
-                new Claim(ClaimTypes.Name, request.UserName)
-            };
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]));
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+			var result = await _signInManager.PasswordSignInAsync(user, request.Password, request.RememberMe, true);
+			if (!result.Succeeded)
+			{
+				return new ApiErrorResult<string>("Đăng nhập không đúng");
+			}
+			var roles = await _userManager.GetRolesAsync(user);
+			var claims = new[]
+			{
+				new Claim(ClaimTypes.Email,user.Email),
+				new Claim(ClaimTypes.GivenName,user.FirstName),
+				new Claim(ClaimTypes.Role, string.Join(";",roles)),
+				new Claim(ClaimTypes.Name, request.UserName)
+			};
+			var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]));
+			var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-            var token = new JwtSecurityToken(_config["Tokens:Issuer"],
-                _config["Tokens:Issuer"],
-                claims,
-                expires: DateTime.Now.AddHours(3),
-                signingCredentials: creds);
+			var token = new JwtSecurityToken(_config["Tokens:Issuer"],
+				_config["Tokens:Issuer"],
+				claims,
+				expires: DateTime.Now.AddHours(3),
+				signingCredentials: creds);
 
-            return new ApiSuccessResult<string>(new JwtSecurityTokenHandler().WriteToken(token));
-        }
+			return new ApiSuccessResult<string>(new JwtSecurityTokenHandler().WriteToken(token));
+		}
 
-        public async Task<ApiResult<UserVm>> GetById(Guid id)
-        {
-            var user = await _userManager.FindByIdAsync(id.ToString());
-            if (user == null)
-            {
-                return new ApiErrorResult<UserVm>("User không tồn tại");
-            }
-            var userVm = new UserVm()
-            {
-                Email = user.Email,
-                PhoneNumber = user.PhoneNumber,
-                FirstName = user.FirstName,
-                Dob = user.Dob,
-                Id = user.Id,
-                LastName = user.LastName,
-                UserName = user.UserName
-            };
-            return new ApiSuccessResult<UserVm>(userVm);
-        }
+		public async Task<ApiResult<bool>> Delete(Guid id)
+		{
+			var user = await _userManager.FindByIdAsync(id.ToString());
+			if (user == null)
+			{
+				return new ApiErrorResult<bool>("User không tồn tại");
+			}
+			var reult = await _userManager.DeleteAsync(user);
+			if (reult.Succeeded)
+				return new ApiSuccessResult<bool>();
 
-        public async Task<ApiResult<PagedResult<UserVm>>> GetUsersPaging(GetUserPagingRequest request)
-        {
-            var query = _userManager.Users;
-            if (!string.IsNullOrEmpty(request.Keyword))
-            {
-                query = query.Where(x => x.UserName.Contains(request.Keyword)
-                 || x.PhoneNumber.Contains(request.Keyword));
-            }
+			return new ApiErrorResult<bool>("Xóa không thành công");
+		}
 
-            //3. Paging
-            int totalRow = await query.CountAsync();
+		public async Task<ApiResult<UserVm>> GetById(Guid id)
+		{
+			var user = await _userManager.FindByIdAsync(id.ToString());
+			if (user == null)
+			{
+				return new ApiErrorResult<UserVm>("User không tồn tại");
+			}
+			var userVm = new UserVm()
+			{
+				Email = user.Email,
+				PhoneNumber = user.PhoneNumber,
+				FirstName = user.FirstName,
+				Dob = user.Dob,
+				Id = user.Id,
+				LastName = user.LastName,
+				UserName = user.UserName
+			};
+			return new ApiSuccessResult<UserVm>(userVm);
+		}
 
-            var data = await query.Skip((request.PageIndex - 1) * request.PageSize)
-                .Take(request.PageSize)
-                .Select(x => new UserVm()
-                {
-                    Email = x.Email,
-                    PhoneNumber = x.PhoneNumber,
-                    UserName = x.UserName,
-                    FirstName = x.FirstName,
-                    Id = x.Id,
-                    LastName = x.LastName
-                }).ToListAsync();
+		public async Task<ApiResult<PagedResult<UserVm>>> GetUsersPaging(GetUserPagingRequest request)
+		{
+			var query = _userManager.Users;
+			if (!string.IsNullOrEmpty(request.Keyword))
+			{
+				query = query.Where(x => x.UserName.Contains(request.Keyword)
+				 || x.PhoneNumber.Contains(request.Keyword));
+			}
 
-            //4. Select and projection
-            var pagedResult = new PagedResult<UserVm>()
-            {
-                TotalRecords = totalRow,
-                PageIndex = request.PageIndex,
-                PageSize = request.PageSize,
-                Items = data
-            };
-            return new ApiSuccessResult<PagedResult<UserVm>>(pagedResult);
-        }
+			//3. Paging
+			int totalRow = await query.CountAsync();
 
-        public async Task<ApiResult<bool>> Register(RegisterRequest request)
-        {
-            var user = await _userManager.FindByNameAsync(request.UserName);
-            if (user != null)
-            {
-                return new ApiErrorResult<bool>("Tài khoản đã tồn tại");
-            }
-            if (await _userManager.FindByEmailAsync(request.Email) != null)
-            {
-                return new ApiErrorResult<bool>("Emai đã tồn tại");
-            }
+			var data = await query.Skip((request.PageIndex - 1) * request.PageSize)
+				.Take(request.PageSize)
+				.Select(x => new UserVm()
+				{
+					Email = x.Email,
+					PhoneNumber = x.PhoneNumber,
+					UserName = x.UserName,
+					FirstName = x.FirstName,
+					Id = x.Id,
+					LastName = x.LastName
+				}).ToListAsync();
 
-            user = new AppUser()
-            {
-                Dob = request.Dob,
-                Email = request.Email,
-                FirstName = request.FirstName,
-                LastName = request.LastName,
-                UserName = request.UserName,
-                PhoneNumber = request.PhoneNumber
-            };
-            var result = await _userManager.CreateAsync(user, request.Password);
-            if (result.Succeeded)
-            {
-                return new ApiSuccessResult<bool>();
-            }
-            return new ApiErrorResult<bool>("Đăng ký không thành công");
-        }
+			//4. Select and projection
+			var pagedResult = new PagedResult<UserVm>()
+			{
+				TotalRecords = totalRow,
+				PageIndex = request.PageIndex,
+				PageSize = request.PageSize,
+				Items = data
+			};
+			return new ApiSuccessResult<PagedResult<UserVm>>(pagedResult);
+		}
 
-        public async Task<ApiResult<bool>> Update(Guid id, UserUpdateRequest request)
-        {
-            if (await _userManager.Users.AnyAsync(x => x.Email == request.Email && x.Id != id))
-            {
-                return new ApiErrorResult<bool>("Emai đã tồn tại");
-            }
-            var user = await _userManager.FindByIdAsync(id.ToString());
-            user.Dob = request.Dob;
-            user.Email = request.Email;
-            user.FirstName = request.FirstName;
-            user.LastName = request.LastName;
-            user.PhoneNumber = request.PhoneNumber;
+		public async Task<ApiResult<bool>> Register(RegisterRequest request)
+		{
+			var user = await _userManager.FindByNameAsync(request.UserName);
+			if (user != null)
+			{
+				return new ApiErrorResult<bool>("Tài khoản đã tồn tại");
+			}
+			if (await _userManager.FindByEmailAsync(request.Email) != null)
+			{
+				return new ApiErrorResult<bool>("Emai đã tồn tại");
+			}
 
-            var result = await _userManager.UpdateAsync(user);
-            if (result.Succeeded)
-            {
-                return new ApiSuccessResult<bool>();
-            }
-            return new ApiErrorResult<bool>("Cập nhật không thành công");
-        }
-    }
+			user = new AppUser()
+			{
+				Dob = request.Dob,
+				Email = request.Email,
+				FirstName = request.FirstName,
+				LastName = request.LastName,
+				UserName = request.UserName,
+				PhoneNumber = request.PhoneNumber
+			};
+			var result = await _userManager.CreateAsync(user, request.Password);
+			if (result.Succeeded)
+			{
+				return new ApiSuccessResult<bool>();
+			}
+			return new ApiErrorResult<bool>("Đăng ký không thành công");
+		}
+
+		public async Task<ApiResult<bool>> Update(Guid id, UserUpdateRequest request)
+		{
+			if (await _userManager.Users.AnyAsync(x => x.Email == request.Email && x.Id != id))
+			{
+				return new ApiErrorResult<bool>("Emai đã tồn tại");
+			}
+			var user = await _userManager.FindByIdAsync(id.ToString());
+			user.Dob = request.Dob;
+			user.Email = request.Email;
+			user.FirstName = request.FirstName;
+			user.LastName = request.LastName;
+			user.PhoneNumber = request.PhoneNumber;
+
+			var result = await _userManager.UpdateAsync(user);
+			if (result.Succeeded)
+			{
+				return new ApiSuccessResult<bool>();
+			}
+			return new ApiErrorResult<bool>("Cập nhật không thành công");
+		}
+	}
 }
